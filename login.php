34228750a4398ada3551d4530a15e1c2dbc6d8f7
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2018-09-02 11:32:55 -0400

    Added PHP MySQL DB connection file and modified login form
    Created separate form file to include on PHP login form page.

--- login.php
+++ login.php
@@ -1,59 +1,40 @@
 <?php
+
+    //Include PHP MySQL DB connection file
+    include './includes/connection.php';
+
+    $errorMessage = "";
+
+    //Check that form has been submitted
     if(isset($_POST['submit'])){
 
         $email = htmlentities($_POST['email']);
         $password = htmlentities($_POST['password']);
 
+        $sql = "SELECT id, email, password FROM users WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+
+            //User account exists in "users" table
+            while($row = mysqli_fetch_assoc($result)) {
+                
+                if(($row['email'] == $email) && ($row['password'] == $password)){
+                    header('Location: ./dashboard/index.php');
+                }
+
+            }
+        } else {
+            $errorMessage = "Please check your username and password and try again.";
+            include './includes/login_form.php';
+        }
+
+        mysqli_close($conn);
+
     }
     else{
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Logbook - Login</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-    <link rel="stylesheet" href="./styles/styles.css" type="text/css">
-
-</head>
-<body>
-
-<!-- Navbar -->
-<?php include './includes/navbar_logged_out.php'; ?>
-<!-- End Navbar -->
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-4 mt-3">
-
-            <h2>Login</h2>
-
-            <form action="#" method="POST">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" placeholder="email@example.com" name="email" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Password</label>
-                    <input type="text" class="form-control" name="password" required>
-                </div>
-
-                <input type="submit" value="Login" class="btn btn-primary" name="submit">
-            </form>
-
-        </div>
-    </div>
-</div>
-
-<?php
+        include './includes/login_form.php';
     }
 ?>
-    
-</body>
-</html>
+
