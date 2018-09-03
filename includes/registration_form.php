bef30213e95463b0b973ac4ee19c9a8bd03c693d
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2018-09-03 18:04:32 -0400

    Edited registration form and created new registration_form.php page to be in "includes" folder.

---
+++ includes/registration_form.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-6 mt-3">
+
+            <h2>Register</h2>
+
+            <div class="form-group">
+                <label for="exampleInputEmail1">First Name</label>
+                <input type="text" class="form-control" aria-describedby="emailHelp" name="firstName">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleInputEmail1">Last Name</label>
+                <input type="text" class="form-control" aria-describedby="emailHelp" name="lastName">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email@example.com">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleInputEmail1">Password</label>
+                <input type="text" class="form-control" aria-describedby="emailHelp" name="password">
+            </div>
+            
+            <div class="form-group">
+                <label for="exampleInputEmail1">Confirm Password</label>
+                <input type="text" class="form-control" aria-describedby="emailHelp" name="confirmPassword">
+            </div>
+
+            <input type="submit" value="Register" class="btn btn-primary" name="submit">
+
+        </div>
+    </div>
+</div>
