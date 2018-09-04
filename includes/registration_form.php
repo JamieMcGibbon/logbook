0f5d0a0bf13ed4c3e9efac19611925be7186f89a
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2018-09-03 20:37:53 -0400

    Added logic to register.php page

--- includes/registration_form.php
+++ includes/registration_form.php
@@ -1,37 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Logbook - Register</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/styles.css" type="text/css">
+
+</head>
+<body>
+
+<!-- Navbar -->
+<?php include './includes/navbar_logged_out.php'; ?>
+<!-- End Navbar -->
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-6 mt-3">
 
             <h2>Register</h2>
 
-            <div class="form-group">
-                <label for="exampleInputEmail1">First Name</label>
-                <input type="text" class="form-control" aria-describedby="emailHelp" name="firstName">
-            </div>
+             <?php 
+                if($errorMessage){
+                    echo "<div class='alert alert-danger'>".$errorMessage."</div>"; 
+                }
+            ?>           
 
-            <div class="form-group">
-                <label for="exampleInputEmail1">Last Name</label>
-                <input type="text" class="form-control" aria-describedby="emailHelp" name="lastName">
-            </div>
 
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email@example.com">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
+            <form action="#" method="POST">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">First Name</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" name="firstName">
+                </div>
 
-            <div class="form-group">
-                <label for="exampleInputEmail1">Password</label>
-                <input type="text" class="form-control" aria-describedby="emailHelp" name="password">
-            </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Last Name</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" name="lastName">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email@example.com" name="email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Password</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" name="password">
+                </div>
+                
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Confirm Password</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" name="confirmPassword">
+                </div>
+
+                <input type="submit" value="Register" class="btn btn-primary" name="submit">
+            </form>
             
-            <div class="form-group">
-                <label for="exampleInputEmail1">Confirm Password</label>
-                <input type="text" class="form-control" aria-describedby="emailHelp" name="confirmPassword">
-            </div>
-
-            <input type="submit" value="Register" class="btn btn-primary" name="submit">
-
         </div>
     </div>
 </div>
+
+</body>
+</html>
--- register.php
+++ register.php
@@ -1,25 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Logbook - Register</title>
+<?php
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-    <link rel="stylesheet" href="./styles/styles.css" type="text/css">
+    //Include PHP MySQL DB connection file
+    include './includes/connection.php';
 
-</head>
-<body>
+    //Error message variable
+    $errorMessage = "";
 
-<!-- Navbar -->
-<?php include './includes/navbar_logged_out.php'; ?>
-<!-- End Navbar -->
+    //Check that registration form has been submitted
+    if(isset($_POST['submit'])){
 
-<!-- Registration Form -->
-<?php include './includes/registration_form.php'; ?>
-<!-- End Registration Form -->
-    
-</body>
-</html>
+        $firstName = htmlentities($_POST['firstName']);
+        $lastName = htmlentities($_POST['lastName']);
+        $email = htmlentities($_POST['email']);
+        $password = htmlentities($_POST['password']);    
+        $confirm_password = htmlentities($_POST['confirmPassword']);
+
+        //Check that the user has filled in all fields
+        if(($firstName != null) && ($lastName != null) && ($email != null) && ($password != null) && ($confirm_password != null)){
+
+            //Check that user data doesn't already exist in database
+            $sql = "SELECT email FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+
+                //User account exists in "users" table; set $errorMessage to let user know that the email
+                //address they entered is already in use
+                $errorMessage = "The email address you entered is already in use. If you already have an account, please <a href='./login.php'>login here</a>.";
+
+                //Show registration form with error message
+                include './includes/registration_form.php';
+
+            }
+            else{
+
+                //User doesn't yet exist in DB. Enter their details into the "users" table of the "logbook" DB
+                
+                //Send the user to the logged in dashboard
+                header('Location: ./dashboard/index.php');
+
+            }
+
+        }
+        else{
+
+            //Set $errorMessage to let user know to fill in all form fields
+            $errorMessage = "Please check that all form fields have been filled out and try again!";
+
+            //Show registration form
+            include './includes/registration_form.php';  
+
+        }
+
+    }
+    else{
+        //Show registration form
+        include './includes/registration_form.php';
+    }
+?>
