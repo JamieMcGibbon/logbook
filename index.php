0f1e6660a9c1ea2b8d00341816cea0240790f4ab
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2018-10-11 22:59:54 -0400

    Added footer to main "index.php" file
    Added footer to main "index.php"/homepage file

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
   <!-- <img src="http://via.placeholder.com/1200x300" class="img-responsive"> -->
   <img src="./includes/homepage_image.jpg" class="img-fluid home_image">
 
-  <div class="container">
+  <div class="container home_main_container">
 
     <div class="row home-text">
       <div class="col-md-12">
@@ -33,7 +33,7 @@
       </div>
     </div>
 
-    <div class="row">
+    <div class="row home-bottom">
       <div class="col-md-4">
         <h1 class="text-center"><i class="fas fa-plane"></i></h1>
         <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quam erat, dapibus non luctus et, porta ac tellus. Aliquam interdum viverra dolor in sodales.</p>
@@ -51,6 +51,11 @@
     </div>
 
   </div>
+
+  <!-- Figure out why styles can't be applied to this div -->
+  <div style="height: 30px;"></div>
+
+  <?php include './includes/footer.php'; ?>
 
 
 
