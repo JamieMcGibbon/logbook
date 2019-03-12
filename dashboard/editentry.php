9878fafef8d870e27b3956f4641b2e3ad0645e63
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2019-03-11 22:06:11 -0400

    Added session, header, etc. code to "editentry.php" page. Added code to get the ID of the logbook entry to be edited.
    Added session, header, etc. code to "editentry.php" page. Added code to get the ID of the logbook entry to be edited.

--- dashboard/editentry.php
+++ dashboard/editentry.php
@@ -1,5 +1,41 @@
 <?php
 
+session_start(); 
 
+//Check that a user is logged in (session variable is set)
+if(!isset($_SESSION['user_id'])){
+
+  //If user isn't logged in, redirect them back to the site homepage
+  header('Location: ../index.php');
+
+}
+
+//Get the ID of the logbook entry that the user would like to edit and store in $entryId variable
+$entryId = $_GET['id'];
+
+//Check that the user ID of the user that's currently logged in matches the user ID associated with the logbook entry being edited
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Flight Logbook - Dashboard</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles/styles.css">
+
+</head>
+<body>
+
+  <!-- Navigation Bar -->
+  <?php include '../includes/navbar_logged_in.php' ?>
+  <!-- / Navigation Bar -->
+
+
+
+
