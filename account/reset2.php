e419830be64bcd10b1b1714785fff532309cf4f3
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2019-04-29 21:59:08 -0400

    Removed "navbar_logged_out.php" include and replaced with HTML code for logged out navbar.
    Removed "navbar_logged_out.php" include and replaced with HTML code for logged out navbar due to addition of "account" directory (which caused relative URLs to not work while using the included file).

--- account/reset2.php
+++ account/reset2.php
@@ -57,9 +57,40 @@ else{
 </head>
 <body>
 
-  <!-- Navigation Bar -->
-  <?php include './includes/navbar_logged_out.php' ?>
-  <!-- / Navigation Bar -->
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="./index.php">Flight Logbook</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="../index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+          </ul>
+
+          <!-- Right Side of Navbar -->
+          <ul class="navbar-nav my-2 my-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="../register.php">Register</a>
+            </li>
+            <li>
+              <a class="nav-link" href="../login.php">Login</a>
+            </li>
+          </ul>
+          <!-- /Right Side of Navbar -->
+
+        </div>
+      </nav>
+      <!-- / Navigation Bar -->
 
   <br />
 
