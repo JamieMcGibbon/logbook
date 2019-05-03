65eb5ef3a7666b0dc0fce8839fa21a23c8d0381c
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2019-05-02 22:46:50 -0400

    Fixed link to the website's homepage on the "reset.php" page.
    Fixed link to the website homepage on the "reset.php" page. (The "Logbook" logo linked to an "index.php" file in the "account" directory, rather than to the main homepage.)

--- account/reset.php
+++ account/reset.php
@@ -22,7 +22,7 @@ session_start();
 
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="./index.php">Flight Logbook</a>
+        <a class="navbar-brand" href="../index.php">Flight Logbook</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
